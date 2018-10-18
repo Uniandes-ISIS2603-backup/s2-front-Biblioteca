e46565884cac4c69d07d001d0b103a06bd5850eb
Juan Nicolás García <jn.garciae@uniandes.edu.co>
2018-10-18 08:33:21 -0500

    Sala

--- src/app/sala/sala.module.ts
+++ src/app/sala/sala.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SalaListComponent } from './sala-list/sala-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [SalaListComponent]
 })
 export class SalaModule { }
