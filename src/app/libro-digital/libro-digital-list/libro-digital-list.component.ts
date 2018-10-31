c1d8420532da5a6ba56afd1a19bc1fbd63b1953b
naalvarado <na.alvarado@uniandes.edu.co>
2018-10-31 09:34:23 -0500

    libro-digital-list

--- src/app/libro-digital/libro-digital-list/libro-digital-list.component.ts
+++ src/app/libro-digital/libro-digital-list/libro-digital-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { libroDigital } from '../../libro-digital/libro-digital';
+import { LibroDigitalService } from '../../libro-digital/libro-digital.service';
+
 @Component({
   selector: 'app-libro-digital-list',
   templateUrl: './libro-digital-list.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibroDigitalListComponent implements OnInit {
 
-  constructor() { }
+  librosDigitales: libroDigital[];
+
+  constructor(private libroDigitalService: LibroDigitalService) { }
+
+  getLibrosDigitales(): void {
+    this.libroDigitalService.getLibrosDigitales().subscribe(librosDigitales => {this.librosDigitales = librosDigitales});
+  }
 
   ngOnInit() {
+    this.getLibrosDigitales();
   }
 
 }
